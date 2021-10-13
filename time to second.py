00cab0f7a50f7da8cd46c507ad2a085d9b914c35
IAM78 <91953698+IAM78@users.noreply.github.com>
2021-10-13 01:48:09 -0700

    Add files via upload

---
+++ time to second.py
@@ -0,0 +1,6 @@
+print("Please enter your time: ")
+hour = int(input('enter hour:'))
+minute = int(input('enter minute:'))
+second = int(input('enter second:'))
+sec = hour*3600 + minute*60 + second
+print('seconds = ', sec)
